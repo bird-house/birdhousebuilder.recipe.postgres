e66389ce0f41283a6e6f112cfd8b2bcec4d08d53
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-03-30 18:40:45 +0200

    update recipe notes

--- birdhousebuilder/recipe/postgres/__init__.py
+++ birdhousebuilder/recipe/postgres/__init__.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (C)2007 'jeanmichel FRANCOIS'
+# Buildout recipe to setup postgres with anaconda for Birdhouse.
+#
+# This recipe is based on https://github.com/makinacorpus/makina.recipe.postgres
+#
+# It is distributed under the GPL license (the same as in makina.recipe.postgres).
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; see the file COPYING. If not, write to the
-# Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """Recipe postgres"""
 import logging
 import os
