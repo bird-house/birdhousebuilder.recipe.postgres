fb2053fcc83044e4d1148f8dc0205897a38f4403
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-03-31 14:09:50 +0200

    refactor pg setup

--- birdhousebuilder/recipe/postgres/__init__.py
+++ birdhousebuilder/recipe/postgres/__init__.py
@@ -14,17 +14,10 @@ from mako.template import Template
 
 from birdhousebuilder.recipe import conda, supervisor
 
-pg_ctl_script = """#!/bin/sh
-PGDATA=%s %s/pg_ctl $@
-"""
-
-psql_script = """#!/bin/sh
-%s/psql $@
-"""
-
+templ_pg_ctl = Template( "${prefix}/bin/pg_ctl -D ${pgdata} ${cmd}" )
+templ_initdb = Template( "${prefix}/bin/initdb --pgdata=${pgdata} ${options}" )
 templ_pg_config = Template(filename=os.path.join(os.path.dirname(__file__), "postgresql.conf"))
-templ_pg_cmd = Template(
-    "${prefix}/bin/postgres -D ${prefix}/var/lib/postgres")
+templ_pg_cmd = Template( "${prefix}/bin/postgres -D ${pgdata}" )
 
 class Recipe(object):
     """This recipe is used by zc.buildout"""
@@ -32,7 +25,6 @@ class Recipe(object):
     def __init__(self, buildout, name, options):
         """options:
         
-          - bin : path to bin folder that contains postgres binaries
           - port : port on wich postgres is started and listen
           - initdb : specify the argument to pass to the initdb command
           - cmds : list of psql cmd to execute after all those init
@@ -55,7 +47,7 @@ class Recipe(object):
             raise RuntimeError('Error running command: %s' % cmd)
 
     def pgdata_exists(self):
-        return os.path.exists(self.options['pgdata']) 
+        return os.path.exists(self.options.get('pgdata')) 
 
     def install(self):
         """installer"""
@@ -79,8 +71,8 @@ class Recipe(object):
             self.buildout,
             'postgres',
             {'program': 'postgres',
-             'command': templ_pg_cmd.render(prefix=self.prefix),
-             'directory': self.options['pgdata']
+             'command': templ_pg_cmd.render( prefix=self.prefix, pgdata=self.options.get('pgdata') ),
+             'directory': self.options.get('pgdata')
              })
         if update == True:
             script.update()
@@ -89,7 +81,6 @@ class Recipe(object):
         return tuple()
     
     def install_pg(self):
-        self.create_bin_scripts()
         #Don't touch an existing database
         if self.pgdata_exists():
             self.stopdb()
@@ -105,7 +96,6 @@ class Recipe(object):
     def update(self):
         """updater"""
         self.logger = logging.getLogger(self.name)
-        self.create_bin_scripts()
         if not self.pgdata_exists():
             self.stopdb()
             self.initdb()
@@ -116,48 +106,31 @@ class Recipe(object):
         return tuple()
 
     def startdb(self):
-        if os.path.exists(os.path.join(self.options.get('pgdata'),'postmaster.pid')):
-            self.system('%s restart'%(self.bin_pg_ctl))
-        else:
-            self.system('%s start'%(self.bin_pg_ctl))
+        cmd = 'start'
+        if self.is_db_started():
+            cmd = 'restart'
+        self.system( templ_pg_ctl.render(prefix=self.prefix, pgdata=self.options.get('pgdata'), cmd='restart') )
         # TODO: check if db is realy up
         time.sleep(10)
 
     def stopdb(self):
-        if os.path.exists(os.path.join(self.options.get('pgdata'),'postmaster.pid')):
-            self.system('%s stop'%(self.bin_pg_ctl))
+        if self.is_db_started():
+            self.system( templ_pg_ctl.render(prefix=self.prefix, pgdata=self.options.get('pgdata'), cmd='stop') )
             time.sleep(10)
 
-    def isdbstarted(self):
-        PIDFILE = os.path.join(self.options.get('pgdata'),'postmaster.pid')
-        return os.path.exists(pg_ctl) and os.path.exists(PIDFILE)
-
-    def create_bin_scripts(self):
-        buildout_bin_path = self.buildout['buildout']['bin-directory']
-        # Create a wrapper script for psql user and admin
-        psql = os.path.join(buildout_bin_path,'psql')
-        script = open(psql , 'w')
-        script.write(psql_script % (self.options.get('bin')))
-        script.close()
-        os.chmod(psql, 0755)
-        pg_ctl = os.path.join(buildout_bin_path,'pg_ctl')
-        script = open(pg_ctl, 'w')
-        script.write(pg_ctl_script % (self.options.get('pgdata'), self.options.get('bin')))
-        script.close()
-        os.chmod(pg_ctl, 0755)
-        self.bin_pg_ctl = pg_ctl
-        self.bin_psql = psql
-        return pg_ctl, psql
+    def is_db_started(self):
+        pidfile = os.path.join( self.options.get('pgdata'), 'postmaster.pid')
+        return os.path.exists( pidfile )
 
     def initdb(self):
-        initdb_options = self.options.get('initdb',None)
-        bin = self.options.get('bin','')
+        initdb_options = self.options.get( 'initdb' )
         if initdb_options and not self.pgdata_exists():
-            self.system('%s %s --pgdata=%s' % (os.path.join(bin, 'initdb'), initdb_options, self.options['pgdata']) )
+            initdb = templ_initdb.render(prefix=self.prefix, options=initdb_options, pgdata=self.options.get('pgdata'))
+            self.system( initdb )
 
     def configure_port(self):
-        result = templ_pg_config.render(port=self.options['port'])
-        output = os.path.join(self.prefix, 'var', 'lib', 'postgres', 'postgresql.conf')
+        result = templ_pg_config.render( port=self.options.get('port') )
+        output = os.path.join(self.pgdata, 'postgresql.conf')
         conda.makedirs(os.path.dirname(output))
 
         with open(output, 'wt') as fp:
@@ -167,11 +140,13 @@ class Recipe(object):
 
     def do_cmds(self):
         cmds = self.options.get('cmds', None)
-        bin = self.options.get('bin')
-        if not cmds: return None
+        if not cmds:
+            return None
         cmds = cmds.split(os.linesep)
         for cmd in cmds:
-            if not cmd: continue
-            try: self.system('%s/%s' % (bin, cmd))
+            if not cmd:
+                continue
+            try:
+                self.system('%s/bin/%s' % (self.prefix, cmd))
             except RuntimeError, e:
                 self.logger.exception('could not run pg setup commands!')
